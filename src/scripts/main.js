2e684e889ae8f67a80c5195bca209ac31d2dd2b0
Vladyslav Kostiuk <K4nezoor@gmail.com>
2025-09-13 14:41:06 +0200

    changed issues

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,14 +5,21 @@ const bigImage = gallery.querySelector('#largeImg');
 const thumbs = document.querySelector('#thumbs');
 
 thumbs.addEventListener('click', (e) => {
-  e.preventDefault();
-
-  const link = e.target.closest('.list-item__link');
+  const link = e.target.closest('a');
 
   if (!link) {
+    return;
   }
+  e.preventDefault();
 
-  const imageSource = link.href;
+  const thumbImg =
+    e.target.tagName === 'IMG' ? e.target : link.querySelector('img');
 
-  bigImage.src = imageSource;
+  bigImage.src = link.href || (thumbImg && thumbImg.src) || bigImage.src;
+
+  if (thumbImg && thumbImg.srcset) {
+    bigImage.srcset = thumbImg.srcset;
+  } else {
+    bigImage.removeAttribute('srcset');
+  }
 });
