c822d63bc99da48286591ca7d4755d3305adfd8f
Vladyslav Kostiuk <K4nezoor@gmail.com>
2025-09-13 14:21:01 +0200

    finished

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery');
+const bigImage = gallery.querySelector('#largeImg');
+const thumbs = document.querySelector('#thumbs');
+
+thumbs.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const link = e.target.closest('.list-item__link');
+
+  if (!link) {
+  }
+
+  const imageSource = link.href;
+
+  bigImage.src = imageSource;
+});
